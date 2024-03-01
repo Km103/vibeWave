9abc07caf8a67bdad9124545c07acd8de048270d
Km103 <21ucs103@lnmiit.ac.in>
2024-03-01 20:39:44 +0530

    added album routes

--- server/src/routes/album.routes.js
+++ server/src/routes/album.routes.js
@@ -7,13 +7,16 @@ import {
     updateAllAlbums,
     updateAllAlbumSongs,
     getAlbum,
+    createAlbum,
 } from "../controllers/album.controllers.js";
 
-router.route("/updateAll").post(updateAllAlbums);
-router.route("/deleteAll").post(deleteAllAlbums);
+// router.route("/updateAll").post(updateAllAlbums);
+// router.route("/deleteAll").post(deleteAllAlbums);
 router.route("/top").get(getTopAlbums);
-router.route("/updateAllSongs").post(updateAllAlbumSongs);
+// router.route("/updateAllSongs").post(updateAllAlbumSongs);
 router.route("/").get(getAlbum);
 ///router.route("/deleteAllSongs").post(deleteAllAlbumsSongs);
+
+router.route("/create").post(createAlbum);
 
 export default router;
